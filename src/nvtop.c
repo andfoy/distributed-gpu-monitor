ea13e604a72865f7876bf78d997cf48487f9f8db
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-09-22 17:35:32 -0500

    Test ZMQ multipart messages

--- src/nvtop.c
+++ src/nvtop.c
@@ -272,7 +272,13 @@ int main (int argc, char **argv) {
       update_window_size_to_terminal_size(interface);
       signal_bits &= ~RESIZE_SIGNAL;
     }
-    zstr_send (push_sock, hostname);
+    zmsg_t *msg = zmsg_new ();
+    zmsg_pushstr(msg, hostname);
+    for(int dev = 0; dev < 0; dev++) {
+      zmsg_addmem(msg, dev)
+      zmsg_addstr(msg, "END")
+      // zstr_send (push_sock, hostname);
+    }
     draw_gpu_info_ncurses(dev_infos, interface);
 
     int input_char = getch();
