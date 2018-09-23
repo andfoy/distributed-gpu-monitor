99cc607a3e56ca43d60a9f7fa9f6537c1e9172b9
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-09-22 19:30:53 -0500

    Add GPU temperature info

--- src/nvtop.c
+++ src/nvtop.c
@@ -277,21 +277,41 @@ int main (int argc, char **argv) {
     char* free_memory_str;
     char* used_memory_str;
     char* total_memory_str;
+    char* gpu_temp_str;
+    char* gpu_temp_slowdown_str;
+    char* gpu_temp_shutdown_str;
+    char* fan_speed_str;
     // zframe_t *frame = zframe_new ("Hello", 5);
     zmsg_pushstr(msg, hostname);
     for(int dev = 0; dev < num_devices; dev++) {
       // zmsg_addmem(msg, dev);
       zmsg_t *mem_msg = zmsg_new();
-      asprintf (&device_num, "%d", dev);
-      zmsg_addstr(msg, device_num);
-      zmsg_addstr(msg,  dev_infos[dev].device_name);
+      zmsg_t *gpu_msg = zmsg_new();
+      zmsg_t *temp_msg = zmsg_new();
+      // GPU information
+      asprintf(&device_num, "%d", dev);
+      zmsg_addstr(gpu_msg, device_num);
+      zmsg_addstr(gpu_msg,  dev_infos[dev].device_name);
+      // Memory information
       asprintf (&used_memory_str, "%llu", dev_infos[dev].used_memory);
       zmsg_addstr(mem_msg, used_memory_str);
       asprintf (&free_memory_str, "%llu", dev_infos[dev].free_memory);
       zmsg_addstr(mem_msg, free_memory_str);
       asprintf (&total_memory_str, "%llu", dev_infos[dev].total_memory);
       zmsg_addstr(mem_msg, total_memory_str);
+      // Temperature/Fan information
+      asprintf (&gpu_temp_str, "%d", dev_infos[dev].gpu_temp);
+      zmsg_addstr(temp_msg, gpu_temp_str);
+      asprintf (&gpu_temp_slowdown_str, "%d", dev_infos[dev].gpu_temp_slowdown);
+      zmsg_addstr(temp_msg, gpu_temp_slowdown_str);
+      asprintf (&gpu_temp_shutdown_str, "%d", dev_infos[dev].gpu_temp_shutdown);
+      zmsg_addstr(temp_msg, gpu_temp_shutdown_str);
+      asprintf (&fan_speed_str, "%d", dev_infos[dev].fan_speed);
+      zmsg_addstr(temp_msg, fan_speed_str);
+      // Pack all messages
+      zmsg_addmsg (msg, &gpu_msg);
       zmsg_addmsg (msg, &mem_msg);
+      zmsg_addmsg (msg, &temp_msg);
       // zmsg_addstr(msg, "END");
       // zstr_send (push_sock, hostname);
     }
