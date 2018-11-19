f89c39651fba2f4d06f4172e1e8da1b6387b7211
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-11-19 11:19:00 -0500

    Add misc print

--- monitor_server/lib/monitor_server.ex
+++ monitor_server/lib/monitor_server.ex
@@ -6,4 +6,8 @@ defmodule MonitorServer do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def func() do
+    IO.puts("sadkasdopas")
+  end
 end
