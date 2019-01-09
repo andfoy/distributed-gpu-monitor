3ebb9b236bb1e454a135f8a7a35e96497c72394d
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-01-08 21:47:10 -0500

    Monitor machine status each 200ms

--- frontend/src/components/GPUAccordion.jsx
+++ frontend/src/components/GPUAccordion.jsx
@@ -9,18 +9,37 @@ import GPUPanel from './GPUPanel';
 export default class GPUAccordion extends React.Component {
     constructor(props) {
         super(props);
+        let machines = process.env.REACT_APP_MACHINES.split(",")
         this.state = {
             gpus: {},
             selectedGPU: null,
             currentMachine: null,
-            tempSeries: []
+            tempSeries: [],
+            downMachines: {},
+            lastContact: machines.reduce((acc, val) => {
+                acc[val] = null;
+                return acc;
+            }, {})
         }
+        this.checkLastContact = this.checkLastContact.bind(this);
         let url = process.env.NODE_ENV !== 'production' ? "ws://127.0.0.1:8001/gpu/" : "ws://marr.uniandes.edu.co/gpu/gpu/"
         this.socket = new Sockette(url, {
             onmessage: this.updateInfo.bind(this),
             onerror: (evt) => { console.log(evt) },
             onclose: () => {console.log("Socket closed")}
         });
+        setInterval(this.checkLastContact, 200);
+    }
+
+    checkLastContact() {
+        var machines = Object.keys(this.state.lastContact);
+        let downMachines = machines.reduce((acc, val) => {
+            if(Date.now() - this.state.lastContact[val] <= 500) {
+                acc[val] = val;
+            }
+            return acc;
+        }, {});
+        this.setState({downMachines: downMachines});
     }
 
     updateInfo(evt) {
@@ -31,6 +50,14 @@ export default class GPUAccordion extends React.Component {
         var currentMachine = this.state.currentMachine;
         var selectedGPU = this.state.selectedGPU;
         var tempSeries = this.state.tempSeries;
+        var lastContact = {...this.state.lastContact};
+        let downMachines = {...this.state.downMachines};
+        lastContact[key] = Date.now();
+
+        if(downMachines[key] !== undefined) {
+            ({key, ...downMachines} = downMachines);
+        }
+
         if(key === this.state.currentMachine) {
             // console.log(msg.gpus);
             // console.log(selectedGPU);
@@ -43,7 +70,8 @@ export default class GPUAccordion extends React.Component {
                 slowTemp: temp.slow_temp});
         }
         this.setState({gpus: update, currentMachine: currentMachine,
-                       selectedGPU: selectedGPU, tempSeries: tempSeries});
+                       selectedGPU: selectedGPU, tempSeries: tempSeries,
+                       lastContact: lastContact, downMachines: downMachines});
     }
 
     updateSelected(machine, gpu) {
@@ -65,11 +93,23 @@ export default class GPUAccordion extends React.Component {
         var available_gpus = Object.keys(this.state.gpus);
         available_gpus = available_gpus.sort();
         var clickFunc = (machine, gpu) => this.updateSelected.bind(this, machine, gpu);
+        var downMachines = Object.keys(this.state.downMachines);
+        var areDownMachines = downMachines.length > 0;
+        var statusMsg = "";
+        if(areDownMachines) {
+            let [first, ...rest] = downMachines;
+            statusMsg = `${first} is down!`;
+            if(rest.length > 0) {
+                let head = downMachines.slice(0, -1);
+                let last = downMachines[downMachines.length - 1];
+                statusMsg = `${head.join(", ")} and ${last} are down!`;
+            }
+        }
         return (
             <Col md={12}>
-                <Alert color="success">
-                    All systems online!
-                    </Alert>
+                <Alert color={"success" ? areDownMachines : "warning"}>
+                    {statusMsg}
+                </Alert>
                 <Row>
                     <Col md={4}>
                         <div>
