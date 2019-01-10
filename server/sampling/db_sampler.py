c29be98aa05d67f4362291deddd38caf50fda096
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-01-09 19:02:35 -0500

    Recreate documents each 24h

--- server/sampling/db_sampler.py
+++ server/sampling/db_sampler.py
@@ -7,22 +7,21 @@ SAMPLE_TEMPLATE = {
     'usage': 0
 }
 
-SAMPLE_VALUE = {
-    'last_time': None,
-    'values': SAMPLE_TEMPLATE,
-    'count': 0
-}
-
 
 class MongoDBSampler:
+    DB = 'gpu'
+    COLLECTION = 'samples'
+
     def __init__(self, servers, mongo_client, queue):
-        self.db = mongo_client.gpu_sampling
+        self.db = mongo_client.gpu
         self.queue = queue
         self.servers = servers
-        self.last_samples = {s: {i: SAMPLE_VALUE for i in range(4)}
-                             for s in servers}
+        gpu_template = {i: SAMPLE_TEMPLATE for i in range(4)}
+        gpu_template['count'] = 0
+        self.last_sample = {s: gpu_template for s in servers}
+        self.current_day = pendulum.today()
 
-    async def create_documents(self, time):
+    async def create_documents(self):
         minutes_template = {
             m: SAMPLE_TEMPLATE for m in range(0, 60, 2)
         }
@@ -32,13 +31,20 @@ class MongoDBSampler:
 
         for server in self.servers:
             entries = [{
-                'timestamp_day': time,
+                'timestamp_day': self.current_day,
                 'machine': server,
                 'gpuid': gpu,
                 'values': hour_template
             } for gpu in range(0, 4)]
-            await self.db.gpu_values.insert_many(entries)
+            await self.db.samples.insert_many(entries)
 
     async def sample(self):
-        current_time = pendulum.today()
-        await self.create_documents(current_time)
+        await self.create_documents()
+        async for info in self.queue:
+            current_time = pendulum.now()
+            time_diff = current_time - self.current_day
+            if time_diff.in_days() >= 1:
+                self.current_day = pendulum.today()
+                await self.create_documents()
+            machine = info['hostname']
+            gpus = info['gpus']
