5b87edc1dc40ba3b734f66757b219e4a0b171c1e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-09-10 23:25:06 -0500

    Remove build files

--- client/debian/dgpumon.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dgpumon package
-#
-0 4	* * *	root	[ -x /usr/bin/dgpumon_maintenance ] && /usr/bin/dgpumon_maintenance
--- client/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dgpumon):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="dgpumon" command="/usr/bin/dgpumon"
